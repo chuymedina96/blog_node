68b0475a098fdc14513169988e5004b6bad8c0fb
Chuy Medina <chuymedina96@gmail.com>
2018-10-13 12:34:13 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,103 @@
+var express             = require("express"),
+    app                 = express(),
+    bodyParser          = require('body-parser'),
+    request             = require("request"),
+    mongoose            = require("mongoose"),
+    methodOverride      = require("method-override"),
+    expressSanitizer    = require("express-sanitizer"),
+    https               = require("https");
+
+var url = process.env.DATABASEURL || "mongodb://localhost/REST";
+console.log(url);
+mongoose.connect(url), {useMongoClient: true};
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
+app.use(express.static("public"));
+app.use(methodOverride("_method"));
+app.set("view engine", "ejs");
+
+
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+var Blog = mongoose.model("Blog", blogSchema);
+//RESTful ROUTER
+app.get("/", function(req, res){
+    res.render("landing");
+});
+app.get("/blogs", function(req, res){
+    Blog.find({}, function(err, blogs){
+        if(err){
+            console.log("Oh no you ran into an error");
+            console.log(err);
+        } else{
+            res.render("index", {blogs: blogs});   
+        }
+    });
+});
+app.post("/blogs", function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+    Blog.create(req.body.blog, function(err, newBlog){
+        if(err){
+            res.render("new");
+        }
+        else{
+            res.redirect("/blogs");
+        }
+    });
+});
+app.get("/blogs/new", function(req, res){
+    res.render("new");
+});
+app.get("/blogs/:id", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            console.log(err);
+            res.redirect("/blogs");
+        } else{
+            res.render("show", {blog: foundBlog}); 
+        }
+    });
+});
+app.get("/blogs/:id/edit", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            console.log(err);
+            res.redirect("/blogs");
+        } else{
+            res.render("edit", {blog: foundBlog}); 
+        }
+    });
+});
+app.put("/blogs/:id", function(req, res){
+    Blog.findByIdAndUpdate(req.params.id,req.body.blog, function(err, updatesBlog){
+        if(err){
+            console.log(err);
+            res.redirect("/edit");
+        } else{
+            res.redirect("/blogs/" + req.params.id);
+        }
+    });
+});
+app.delete("/blogs/:id", function(req, res){
+    Blog.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            res.redirect("/blogs");
+        } else{
+            res.redirect("/blogs");
+        }
+    });
+});
+app.get("*", function(req, res){
+    res.send("404, go back now!! before its too late!");
+});
+//SERVER and Heroku Dyno
+setInterval(function() {
+    https.get("https://restful-node-blog.herokuapp.com/");
+}, 300000); // pings the server every 5 minutes preventing dyno sleep (300000)
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Server has started! Hurray!");
+});
